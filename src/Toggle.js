3728d9ac2e47f5e3d6674ad98a82429fbe533c2f
maniac23 <tukaym@gmail.com>
2019-06-27 21:30:21 +0300

    Transition between components

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,25 +2,67 @@ import React, { useState } from 'react';
 import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const transition = useTransition(isToggled, null, {
+  const [items, setItems] = useState([
+    {
+      letter: 'S',
+      key: 1
+    },
+    {
+      letter: 't',
+      key: 2
+    },
+    {
+      letter: 'r',
+      key: 3
+    },
+    {
+      letter: 'i',
+      key: 4
+    },
+    {
+      letter: 'n',
+      key: 5
+    },
+    {
+      letter: 'g',
+      key: 6
+    }
+  ]);
+  // const transition = useTransition(isToggled, null, {
+  //   from: { opacity: 0, position: 'absolute' },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 }
+  // });
+  const transition = useTransition(items, item => item.key, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 }
   });
 
+  // return (
+  //   <div style={{ position: 'relative' }}>
+  //     {transition.map(({ item, key, props }) =>
+  //       item ? (
+  //         <animated.h1 style={props}>Hello</animated.h1>
+  //       ) : (
+  //         <animated.h1 style={props}>World</animated.h1>
+  //       )
+  //     )}
+
+  //     <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+  //   </div>
+  // );
   return (
     <div>
-      {transition.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.h1 key={key} style={props}>
-              Hello
-            </animated.h1>
-          )
-      )}
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
 
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      <button onClick={() => setItems([{ letter: 'S', key: 1 }])}>
+        Toggle
+      </button>
     </div>
   );
 };
