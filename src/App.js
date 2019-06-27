9ef70af1871c3561391e8148f8bbcce2901f4a0e
Mikhail Tukai <m.tukai@reply.com>
2019-06-27 18:02:46 +0300

    Cart animation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import logo from './logo.svg';
 import './App.css';
 import Toggle from './Toggle';
 import Nav from './Nav';
-
+import Checkout from './Checkout';
 const App = () => {
   const [isNavOpen, setNavOpen] = useState(false);
-  const navAnimation = useSpring({ transform: isNavOpen ? 'translate3d(0, 0, 0) scale(1)' : 'translate3d(100%, -100%, 0) scale(0.6)' });
+
   const fade = useSpring({
     from: {
       opacity: 0
@@ -24,10 +24,11 @@ const App = () => {
         <button onClick={() => setNavOpen(!isNavOpen)} className='menu-button'>
           Menu
         </button>
-        <Nav style={navAnimation} />
+        {/* <Nav style={navAnimation} /> */}
       </header>
       <main>
         <Toggle />
+        <Checkout isOpen={isNavOpen} />
       </main>
     </animated.div>
   );
