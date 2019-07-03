77d30ad882071bdc986b86316dd16a8db1528546
maniac23 <tukaym@gmail.com>
2019-07-03 21:38:54 +0300

    refactoring

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { animated, useTrail } from 'react-spring';
 
 const Boxes = ({ isToggled }) => {
-  const [on, toggled] = useState(false);
   const items = [0.5, 0.7, 0.9, 1, 0.1];
-  console.log(isToggled);
   const trail = useTrail(items.length, {
     transform: isToggled ? 'trasnlate3d(0, 0, 0)' : 'translate3d(-50%, 0, 0)',
     opacity: isToggled ? 1 : 0
